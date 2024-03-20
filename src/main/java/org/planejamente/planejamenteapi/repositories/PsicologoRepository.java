27e70170958b84e9b68eaaa31407d96a6e93fd39
Matheus Nunes <matheus.nunes@sptech.school>
2024-03-20 20:29:13 -0300

    Adicionando interface de metodos genericos das controllers e adicionando sortStrategy

--- src/main/java/org/planejamente/planejamenteapi/repositories/PsicologoRepository.java
+++
@@ -1,9 +0,0 @@
-package org.planejamente.planejamenteapi.repositories;
-
-import org.planejamente.planejamenteapi.Psicologo;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PsicologoRepository extends JpaRepository<Psicologo, Integer> {
-    Psicologo save(Psicologo psicologo);
-}
