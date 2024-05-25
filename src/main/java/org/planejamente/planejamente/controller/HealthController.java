442ee33084b6da92a6f232ed2550917aecd1b981
Icaro Bezerra <icaro.bezerra@sptech.school>
2024-05-25 22:51:11 +0000

    add health check

---
+++ src/main/java/org/planejamente/planejamente/controller/HealthController.java
@@ -0,0 +1,18 @@
+package org.planejamente.planejamente.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-bro")
+@CrossOrigin
+public class HealthController {
+
+    @GetMapping()
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("All right!");
+    }
+}
--- src/main/java/org/planejamente/planejamente/infra/security/SecurityConfigurations.java
+++ src/main/java/org/planejamente/planejamente/infra/security/SecurityConfigurations.java
@@ -38,6 +38,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.GET, "/pacientes/**").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/pacientes/csv").permitAll()
                         .requestMatchers(HttpMethod.POST, "/consultas").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/hello-bro").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
