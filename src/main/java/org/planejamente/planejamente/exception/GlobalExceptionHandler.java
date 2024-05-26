07d152ac7bb63d0248a54cbda46b3a93747e1724
Matheus Nunes <matheus.nunes@sptech.school>
2024-05-26 20:07:17 -0300

    Adicionando tratamento de exception de login

--- src/main/java/org/planejamente/planejamente/exception/GlobalExceptionHandler.java
+++ src/main/java/org/planejamente/planejamente/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.planejamente.planejamente.exception;
 import jakarta.xml.bind.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,4 +17,10 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity handleResponseStatusException(ResponseStatusException ex) { return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage()); }
+
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity handleAuthenticationException(AuthenticationException ex) {
+        return ResponseEntity.status(401).build();
+    }
 }
